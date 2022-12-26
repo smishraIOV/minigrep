5f5b2025ae9214027b9e9cbf18ccef4e70a5f3e7
optimalbrew <petecarkeek@gmail.com>
2022-12-26 14:37:39 -0800

    refactor for better error handling using Result<>. Lots of small changes, need to pay attention to details.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     //aside: run with cargo run -- "the" "./poem.txt"
 
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).
+                     unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -23,13 +28,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // "new" is now "build" and has a new signature using Result<>
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3{
-	    panic!{"not enough arguments"};
+	    return Err("not enough arguments");
 	}
         let query = args[1].clone(); // the program's name is &arg[0]
         let file_path = args[2].clone();
         
-        Config {query, file_path }
+        Ok(Config {query, file_path })
     }
 }
