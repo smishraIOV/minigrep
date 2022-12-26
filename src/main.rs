45714316572e5ba9132c0c421300b09b9c3fb298
optimalbrew <petecarkeek@gmail.com>
2022-12-26 14:14:00 -0800

    refactor again, use a struct to avoid deconstructing tuple

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,27 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
     
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
     
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With test: \n{contents}");
 }
 
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str){
-    let query = &args[1]; // the program's name is &arg[0]
-    let file_path = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    // using clone copies stuff.. so performance penalty. Fix with experience
+    // a working program is higher priority now than optimization
+    let query = args[1].clone(); // the program's name is &arg[0]
+    let file_path = args[2].clone();
     
-    (query, file_path)    
+    Config {query, file_path }    
 }
