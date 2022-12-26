44d4331f5b82defd252310a34c1a47b1f5badd8c
optimalbrew <petecarkeek@gmail.com>
2022-12-26 13:09:47 -0800

    print the args explicitly

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args); // print for debugging
+
+    //dbg!(args); // print for debugging, removed later
+    
+    let query = &args[1]; // the program's name is &arg[0]
+    let file_path = &args[2];
+    
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
