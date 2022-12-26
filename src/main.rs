fe9dcf153d6ce296ceb457bec945cca823d38383
optimalbrew <petecarkeek@gmail.com>
2022-12-26 15:19:55 -0800

    extract logic into a library file with a public API

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     //aside: run with cargo run -- "the" "./poem.txt"
@@ -17,43 +17,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     
-   // not using unwrap here since this is for error handling only
-   // unwrap is useful if we plan to do something with returned object
-   if let Err(e) =  run(config) {
+   if let Err(e) =  minigrep::run(config) {
        println!("Application error: {e}");
        process::exit(1);
    }
-}
-
-// extract this part from main as run
-// use Result to avoid panic due to 'expext' from earlier version
-// can return a dynamic error train
-// if all is well, then we return (), i.e. not really returning a type.. just side effects
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // "?" will return error instead of `expect` which panics
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With test: \n{contents}");
-
-    Ok(())
-}
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    // "new" is now "build" and has a new signature using Result<>
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3{
-	    return Err("not enough arguments");
-	}
-        let query = args[1].clone(); // the program's name is &arg[0]
-        let file_path = args[2].clone();
-        
-        Ok(Config {query, file_path })
-    }
 }
