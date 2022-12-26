0f551806cd5ac4fe9f038e7b118805868ccccd36
optimalbrew <petecarkeek@gmail.com>
2022-12-26 13:22:25 -0800

    add a file and verify it's readable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,7 +8,14 @@ fn main() {
     
     let query = &args[1]; // the program's name is &arg[0]
     let file_path = &args[2];
+
+    //run with cargo run -- "the" "./poem.txt"
     
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+    
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With test: \n{contents}");
 }
