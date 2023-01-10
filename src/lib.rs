111e7f0d5e63c8d80276148dcd0369e48a13089b
shreemoy mishra <petecarkeek@gmail.com>
2023-01-09 17:45:45 -0800

    new failed test by coipying existing one and changing the case to trip the test

--- src/lib.rs
+++ src/lib.rs
@@ -52,8 +52,18 @@ mod  tests {
     use super::*;
 
     #[test]
-    fn one_result(){
+    fn case_sensitive(){
         let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+        assert_eq!(vec!["        safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "duCt";
         let contents = "\
         Rust:
         safe, fast, productive.
